16bcc6a8dca4c092a67ca146c29d8e69dcfc03e9
brandyaptx <lwp1225@gmail.com>
2017-03-14 16:07:34 +0800

    Add files via upload

---
+++ src/main/java/com/easy/custom/tools/Trietest.java
@@ -0,0 +1,45 @@
+import org.ahocorasick.trie.Trie;
+
+import java.io.*;
+import java.util.LinkedList;
+
+
+/**
+ * Created by lwp on 2017/3/14.
+ */
+public class Trietest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br;
+        br = new BufferedReader(new InputStreamReader(new FileInputStream("E:\\eclipse\\workspace\\actest\\src\\main\\resources\\dictionary.txt")));
+        String line;
+
+
+        LinkedList<String> dictionary = new LinkedList<String>();
+        while ((line = br.readLine()) != null)
+        {
+            dictionary.add(line);
+        }
+        br.close();
+        long start = System.currentTimeMillis();
+        Trie unicodeTrie  = Trie.builder().addKeywords(dictionary).build();
+        System.out.printf("unicodeTrie adding time:%dms%n", System.currentTimeMillis() - start);
+
+
+
+        int pressure = 100000;
+        String text = "http://share.iclient.ifeng.com/news/sharenews.f?aid=82969287/";
+
+        start = System.currentTimeMillis();
+        System.out.println(unicodeTrie.parseText(text));
+        System.out.printf("asciiTrie building time:%dms%n", System.currentTimeMillis() - start);
+
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < pressure; ++i)
+        {
+            unicodeTrie.parseText(text);
+        }
+        System.out.printf("unicodeTrie used time:%dms%n", System.currentTimeMillis() - start);
+    }
+
+}
